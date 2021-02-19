84a2fa8460d83bf5fd51c9f37698efef7a424e40
Kyle Dushman <kdushman@indiana.edu>
2021-02-19 15:49:17 -0800

    debug

--- src/controllers/securities.js
+++ src/controllers/securities.js
@@ -348,6 +348,14 @@ export async function getSecurityPerformance(ticker) {
       todayperf = ((lastPrice.last_price || earliest.value) / earliest.value - 1) * 100;
     }
 
+    console.log("what is inifinty");
+    console.log("today: ", todayperf);
+    console.log("week: ", (earliest.value / weekPrice.value - 1) * 100);
+    console.log("2week: ", (earliest.value / twoweekPrice.value - 1) * 100);
+    console.log("1month: ", (earliest.value / monthPrice.value - 1) * 100);
+    console.log("3month: ", (earliest.value / threemonthPrice.value - 1) * 100);
+    console.log("1year: ", (earliest.value / latest.value - 1) * 100);
+
     let perf = {
       price_percent_change_today: todayperf,
       price_percent_change_7_days: (earliest.value / weekPrice.value - 1) * 100,
